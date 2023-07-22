247262753dee26ae359144fe32cfe241bcfb01fb
myrol <posthomerik@gmail.com>
2023-07-22 19:37:42 +0200

    Day 1 Part 1 Solved

--- 2022/1/1.py
+++ 2022/1/1.py
@@ -1,5 +1,18 @@
 def main():
-    print("Hello World")
+    with open('input.txt', 'r') as fp:
+        lines = fp.readlines()
+
+    most_calories = 0
+    current_calories_total = 0
+    for line in lines:
+        if line != '\n':
+            current_calories_total += int(line)
+            continue
+        if current_calories_total > most_calories:
+            most_calories = current_calories_total
+        current_calories_total = 0
+
+    print(most_calories)
 
 
 if __name__ == '__main__':
