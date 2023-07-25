e85007e37eca17942e6fcffabe1f63ef5574830e
myrol <posthomerik@gmail.com>
2023-07-25 13:59:51 +0200

    Solved Day 6

--- 2022/6/6.py
+++ 2022/6/6.py
@@ -1,5 +1,12 @@
 def main():
-    print("Hello World")
+    with open("input.txt", "r") as fp:
+        datastream = fp.read()
+
+    message_len = 14
+    i = 0
+    while len(set(datastream[i:i+message_len])) != message_len:
+        i += 1
+    print(i+message_len)
 
 
 if __name__ == "__main__":
